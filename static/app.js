7a5c14967e50ddf10909185585668ba8f5970a7b
aaron-mancini <aaronmancini2301@gmail.com>
2021-09-28 15:58:28 -0400

    added autocomplete to search

--- static/app.js
+++ static/app.js
@@ -1,8 +1,5 @@
-searchTerms = ["NEE", "AAPL", "AMD", "MSFT"];
-$( function() {
-    $( "#search" ).autocomplete({source: searchTerms});
-})
 
+// Add or remove stock from watchlist.
 $("#add-stock").on("click", "p", async function(evt) {
     evt.preventDefault();
     
@@ -15,3 +12,46 @@ $("#add-stock").on("click", "p", async function(evt) {
         params: {watchlist: $watchlistId, ticker: $ticker}})
     
 });
+
+// debouncing functino
+const debounce = (func, delay) => {
+    let debounceTimer
+    return function() {
+        const context = this
+        const args = arguments
+            clearTimeout(debounceTimer)
+                debounceTimer
+            = setTimeout(() => func.apply(context, args), delay)
+    }
+}
+
+debounce($(function(){
+    $('#search').autocomplete({
+        source: async function (query, done) {
+            // Do Ajax call or lookup locally, when done,
+            // call the callback and pass your results:
+            // let searchTerm = this.value;
+            
+            const res = await axios.request({
+                method: 'GET',
+                url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete',
+                params: {q: query.term, region: 'US'},
+                headers: {
+                    'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+                    'x-rapidapi-key': apiKey
+                }
+            })
+            let stockSuggestions = res.data.quotes;
+            let searchTerms = [];
+            for (let stock of stockSuggestions) {
+                if (stock.exchange == 'NMS' || stock.exchange == 'NYQ') {
+                    searchTerms.push({ label : `${stock.shortname} | ${stock.symbol}`, value : stock.symbol })
+                };
+            };
+    
+    
+            done(searchTerms);
+        },
+        
+    });
+}), 500);
